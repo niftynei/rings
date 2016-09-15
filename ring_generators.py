4b418780de81640ff305430cd7bb905c907796c7
Dan Puttick <dputtick@users.noreply.github.com>
2016-09-15 17:05:29 -0400

    adding spaces before comments

--- ring_generators.py
+++ ring_generators.py
@@ -2,13 +2,14 @@ import itertools
 import time
 
 
-# int_in needs to be set to something for the "priming" step
-def pass_message(int_in = -1):
+def pass_message():
+    int_in = -1
     while True:
         int_in = yield int_in
 
 
-def pass_and_decrement(int_in = -1):
+def pass_and_decrement():
+    int_in = -1
     while True:
         int_in -= 1
         int_in = yield int_in
@@ -16,11 +17,13 @@ def pass_and_decrement(int_in = -1):
 
 def main():
     nodes = []
+
     # set up and prime our starter object
     starter = pass_and_decrement()
     starter.send(None)
     nodes.append(starter)
     print("made starter")
+
     # make and prime all of our nodes
     for _ in range(10000):
         node = pass_message()
@@ -30,6 +33,7 @@ def main():
     message = 10000
     # start tracking time
     start = time.time()
+
     # itertools.cycle gives us an infinite cycle of nodes
     for gen in itertools.cycle(nodes):
         message = gen.send(message)
